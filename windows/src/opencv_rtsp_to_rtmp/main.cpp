b528590faf4c83a0d07893357701ddc011f3e8d5
mlfcjob@163.com <mlfcjob@163.com>
2017-11-14 17:26:17 +0800

    Open a encoder codec and start encode.
    Open a encoder codec and start encode.
    
    Signed-off-by: mlfcjob@163.com <mlfcjob@163.com>

--- windows/src/opencv_rtsp_to_rtmp/main.cpp
+++ windows/src/opencv_rtsp_to_rtmp/main.cpp
@@ -21,11 +21,15 @@ int main(int argc, char *argv[])
 {
 
 	//char *inUrl = "rtsp:://test:test123456@192.168.1.64"; //hikon
-	char *inUrl = "rtsp://184.72.239.149/vod/mp4://BigBuckBunny_175k.mov"; //big bunny
+	//char *inUrl = "rtsp://184.72.239.149/vod/mp4://BigBuckBunny_175k.mov"; //big bunny
 	//char *inUrl = "rtmp://live.hkstv.hk.lxdns.com/live/hks";
+	char *inUrl = "F:\\FFOutput\\2504.mkv";
 	//nginx-rtmp 直播服务器
 	char *outUrl = "rtmp://192.168.1.104/live";
-	
+
+	//注册所有编解码器信息
+	avcodec_register_all();
+
 	VideoCapture cam;
 	Mat frame;
 	namedWindow("video");
@@ -37,9 +41,8 @@ int main(int argc, char *argv[])
 	//编码器上下文
 	AVCodecContext   *vc = NULL;
 
-	try {
-		av_register_all();
 
+	try {
 		/// 1. 打开像机(opencv)
 		cam.open(inUrl);
 
@@ -50,6 +53,7 @@ int main(int argc, char *argv[])
 
 		int inWidth = cam.get(CAP_PROP_FRAME_WIDTH);
 		int inHeight = cam.get(CAP_PROP_FRAME_HEIGHT);
+		int fps = cam.get(CAP_PROP_FPS);
 
 
 		/// 2. init convert context
@@ -65,6 +69,11 @@ int main(int argc, char *argv[])
 		{
 			throw exception("sws_getCacheContext faied");
 		}
+
+		AVPacket pack;
+		memset(&pack, 0, sizeof(pack));
+
+		int vpts = 0;
 
 		for (;;)
 		{
@@ -94,13 +103,13 @@ int main(int argc, char *argv[])
 			int ret = av_frame_get_buffer(yuv, 32);
 			if (ret != 0)
 			{
-				char buf[1024] = {0};
+				char buf[1024] = { 0 };
 				av_strerror(ret, buf, sizeof(buf));
 				throw exception("buf");
 			}
 
 			//输入的数据结构
-			uint8_t  *indata[AV_NUM_DATA_POINTERS] = {0};
+			uint8_t  *indata[AV_NUM_DATA_POINTERS] = { 0 };
 			// interlaced:BGR BGR BGR BGR
 			// planed: indata[0]: BBBB  indata[1]: GGGG indata[2]:RRRR
 			indata[0] = frame.data;
@@ -108,20 +117,19 @@ int main(int argc, char *argv[])
 			//一行的数据的字节数（width）
 			insize[0] = frame.cols * frame.elemSize();
 			int h = sws_scale(vsc, indata, insize, 0, frame.rows, // 源数据
-				              yuv->data, yuv->linesize);
+				yuv->data, yuv->linesize);
 
 			if (h <= 0)
 			{
 				continue;
 			}
-			cout << h << " " << flush;
 
 			/// 4 初始化编码器上下文
 			// a 找到编码器
 			AVCodec *codec = avcodec_find_encoder(AV_CODEC_ID_H264);
 			if (!codec)
 			{
-				throw exception("can not finde h264 encoder!");
+				throw exception("can not find h264 encoder!");
 			}
 
 			// b 创建编码器上下文
@@ -131,7 +139,53 @@ int main(int argc, char *argv[])
 				throw exception("avcodec_alloc_context3 failed!");
 			}
 
-			// c 打开编码器上下文
+			// c 配置编码器参数
+			vc->flags |= AV_CODEC_FLAG_GLOBAL_HEADER;  //全局参数
+			vc->codec_id = codec->id;
+			vc->thread_count = 8;
+
+			// only for video
+			vc->bit_rate = 50 * 1024 * 8;      // bits size per sec after compressed : 50kB
+			vc->width = inWidth;
+			vc->height = inHeight;
+			vc->time_base = { 1, fps };  // pts * 1 / fps = second 
+			vc->framerate = {fps, 1};
+
+			// 画面组的大小，多少帧一个关键帧，
+			//设得越大，压缩率越高，同时画质也会更容易损失，性能消耗也会更大
+			vc->gop_size = 5;
+			vc->max_b_frames = 0; // B 帧设为0， 这种情况下pts和dts就会一致
+			vc->pix_fmt = AV_PIX_FMT_YUV420P;
+
+			//d 打开编码器上下文
+			ret = avcodec_open2(vc, NULL, NULL);
+			if (ret != 0)
+			{
+				char buf[1024] = {0};
+				av_strerror(ret, buf, sizeof(buf) - 1);
+				throw exception(buf);
+			}
+
+			cout << "avcodec Open 2 encoder success." << endl;
+
+			// h264 encode
+			yuv->pts = vpts++;
+
+			ret = avcodec_send_frame(vc, yuv);
+			if (ret != 0)
+			{
+				continue;
+			}
+
+			ret = avcodec_receive_packet(vc, &pack);
+			if (ret != 0 || pack.size > 0)
+			{
+				cout << "*" << pack.size << flush;
+			} 
+			else 
+			{
+				continue;
+			}
 
 			// push media
 		}
