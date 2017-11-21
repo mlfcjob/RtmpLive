3073410a79063613fa23f9ce1413bdefda4c85fe
mlfcjob@163.com <mlfcjob@163.com>
2017-11-21 17:36:01 +0800

    Finish encode audio and push audio to rtmp server.
    Finish encode audio and push audio to rtmp server.
    
    Signed-off-by: mlfcjob@163.com <mlfcjob@163.com>

--- windows/src/qt_audio_rtmp/main.cpp
+++ windows/src/qt_audio_rtmp/main.cpp
@@ -1,15 +1,19 @@
 #include <QtCore/QCoreApplication>
 #include <qaudioinput.h>
 #include <iostream>
+#include <QThread>
 
 extern "C"
 {
 #include <libswresample/swresample.h>
+#include <libavcodec/avcodec.h>
+#include <libavformat/avformat.h>
 }
 
 #pragma comment(lib, "swresample.lib")
 #pragma comment(lib, "avutil.lib")
-
+#pragma comment(lib, "avcodec.lib")
+#pragma comment(lib, "avformat.lib")
 
 using namespace std;
 
@@ -17,10 +21,20 @@ int main(int argc, char *argv[])
 {
 	QCoreApplication a(argc, argv);
 	
+	const char *outUrl = "rtmp://192.168.103.139/live";
+	//const char *outUrl = "r.mp3";
+
+
 	int sampleRate = 44100;
 	int channels = 2;
-	int sampleSize = 2;
+	int sampleByte = 2;
+	AVSampleFormat inSampleFmt = AV_SAMPLE_FMT_S16;
+	AVSampleFormat outSampleFmt = AV_SAMPLE_FMT_FLTP;
 
+	avcodec_register_all();
+
+	av_register_all();
+	avformat_network_init();
 
 	// 1 qt音频开始录制
 	QAudioFormat fmt;
@@ -48,8 +62,8 @@ int main(int argc, char *argv[])
 	// 2 音频重采样上下文初始化
 	SwrContext *asc = NULL;
 	asc = swr_alloc_set_opts(asc, 
-		              av_get_default_channel_layout(channels), AV_SAMPLE_FMT_FLTP/*for aac*/, sampleRate,  //输出格式
-		              av_get_default_channel_layout(channels), AV_SAMPLE_FMT_S16, sampleRate, //输入格式
+		              av_get_default_channel_layout(channels), outSampleFmt/*for aac*/, sampleRate,  //输出格式
+		              av_get_default_channel_layout(channels), inSampleFmt, sampleRate, //输入格式
 		              0, 0);
 
 	if (!asc)
@@ -69,15 +83,182 @@ int main(int argc, char *argv[])
 	}
 
 	cout << "swr init success." << endl;
+	// 3 音频重采样输出空间分配
+	AVFrame *pcm = av_frame_alloc();
+	pcm->format = outSampleFmt;
+	pcm->channels = channels;
+	pcm->channel_layout = av_get_default_channel_layout(channels);
+	pcm->nb_samples = 1024;  // 一帧音频一个通道的采样数量
+	ret = av_frame_get_buffer(pcm, 0);  // 给pcm分配存储空间
+	if (ret != 0)
+	{
+		char err[1024] = { 0 };
+		av_strerror(ret, err, sizeof(err));
+		getchar();
+		return -1;
+	}
 
-	char buf[4096] = { 0 };
+	// 4 初始化音频编码器
+	AVCodec  *codec = avcodec_find_encoder(AV_CODEC_ID_AAC);
+	if (!codec)
+	{
+		cout << "avcodec_find_encoder AV_CODEC_ID_AAC failed" << endl;
+		getchar();
+		return -1;
+	}
+
+	AVCodecContext *ac = avcodec_alloc_context3(codec);
+	if (!ac)
+	{
+		cout << "avcodec_alloc_context3 failed" << endl;
+		getchar();
+		return -1;
+	}
+
+	cout << "avcodec_alloc_context3 success" << endl;
+
+	ac->flags |= AV_CODEC_FLAG_GLOBAL_HEADER;
+	ac->thread_count = 8;
+	ac->bit_rate = 40000;
+	ac->sample_rate = sampleRate;
+	ac->sample_fmt = outSampleFmt;
+	ac->channels = channels;
+	ac->channel_layout = av_get_default_channel_layout(channels);
+
+	// 打开音频编码器
+	ret = avcodec_open2(ac, 0, 0);
+	if (ret != 0)
+	{
+		char err[1024] = { 0 };
+		av_strerror(ret, err, sizeof(err));
+		getchar();
+		return -1;
+	}
+
+	std::cout << "avcodec open success" << endl;
+
+	// 5 输出封装器和音频流配置
+	// a 创建输出封装器上下文
+	AVFormatContext *ic = NULL;
+	ret = avformat_alloc_output_context2(&ic, 0, "flv", outUrl);
+	if (ret != 0)
+	{
+		char err[1024] = { 0 };
+		av_strerror(ret, err, sizeof(err));
+		getchar();
+		return -1;
+
+	}
+
+	// b 添加音频流
+	AVStream *as = avformat_new_stream(ic, NULL);
+	if (!as)
+	{
+		char err[1024] = { 0 };
+		av_strerror(ret, err, sizeof(err));
+		getchar();
+		return -1;
+	}
+	as->codecpar->codec_tag = 0;
+
+	//从编码器复制参数
+	avcodec_parameters_from_context(as->codecpar, ac);
+	av_dump_format(ic, 0, outUrl, 1);
+
+
+	//6 打开rtmp 的网络输出IO
+	ret = avio_open(&ic->pb, outUrl, AVIO_FLAG_WRITE);
+	if (ret != 0)
+	{
+		char err[1024] = { 0 };
+		av_strerror(ret, err, sizeof(err));
+		getchar();
+		return -1;
+	}
+
+	// 写入封装头
+	ret = avformat_write_header(ic, NULL);
+	if (ret != 0)
+	{
+		char err[1024] = { 0 };
+		av_strerror(ret, err, sizeof(err));
+		getchar();
+		return -1;
+	}
+
+	// 一次读取一帧音频的字节数
+	int readSize = pcm->nb_samples * channels * sampleByte;
+	char *buf = new char[readSize];
+
+
+	int apts = 0;
+	AVPacket pkt = {0};
+
 	for (;;)
 	{
-
-		if (input->bytesReady() > 4096)
+		if (input->bytesReady() < readSize)
 		{
-			cout << "read len: "<< io->read(buf, sizeof(buf)) << endl;
+			QThread::msleep(1);
+			continue;
+		}
+
+		int size = 0;
+		while (size != readSize)
+		{
+			int len =  io->read(buf + size, readSize - size);
+			if (len < 0)
+			{
+				break;
+			}
+			size += len;
+		}
+
+		if (size != readSize)
+		{
+			continue;
+		}
+
+		//已经读一帧源数据
+		// cout << size << "";
+		//重采样源数据
+		const uint8_t *indata[AV_NUM_DATA_POINTERS] = {0};
+		indata[0] = (uint8_t *)buf;
+		int len = swr_convert(asc, pcm->data, pcm->nb_samples,  //输出参数：存储地址和样本数量
+			        indata, pcm->nb_samples);
+
+		// pts运算
+		// nb_sample / smaple_rate  = 一帧音频的秒数
+		// timebase pts = sec * timebase.den
+		pcm->pts = apts;
+		apts += av_rescale_q(pcm->nb_samples, { 1, sampleRate }, ac->time_base);
+
+
+		ret = avcodec_send_frame(ac, pcm);
+		if (ret != 0)
+		{
+			continue;
+		}
+
+		av_packet_unref(&pkt);
+		ret = avcodec_receive_packet(ac, &pkt);
+		if (ret != 0)
+		{
+			continue;
+		}
+		cout << pkt.size << " " << flush;
+
+		pkt.pts = av_rescale_q(pkt.pts, ac->time_base, as->time_base);
+		pkt.dts = av_rescale_q(pkt.dts, ac->time_base, as->time_base);
+		pkt.duration = av_rescale_q(pkt.duration, ac->time_base, as->time_base);
+		ret = av_interleaved_write_frame(ic, &pkt);
+
+		if (ret == 0)
+		{
+			cout << "#" << flush;
 		}
 	}
+
+	delete buf;
+	getchar();
 	return a.exec();
 }
